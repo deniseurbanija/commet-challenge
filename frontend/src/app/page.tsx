4f14857bb648d8cf904203e552a0ac0b7f50ca9d
deniseurbanija <deniseurbanija1@gmail.com>
2025-03-26 20:42:49 -0300

    styles

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -20,31 +20,33 @@ export default function Home() {
     setTotalCommissions(total);
   }, []);
   return (
-    <div className="min-h-screen text-black bg-slate-50 p-6 md:p-10">
+    <div className="min-h-screen text-[#0f172a] bg-gradient-to-t from-gray-300 to-gray-50 p-6 md:p-10">
       <div className="max-w-5xl mx-auto space-y-8">
-        <div className="flex flex-col items-center justify-center text-center space-y-2">
-          <h1 className="text-3xl font-bold tracking-tight">CRM Data Parse</h1>
+        <div className="flex flex-col items-center justify-center text-center space-y-2 mt-24">
+          <h1 className="text-3xl font-bold tracking-widest">
+            Commet - Gestión de Deals
+          </h1>
         </div>
-        <div className="bg-white p-6 rounded-lg shadow-sm">
+        <div className="bg-white p-8 rounded-2xl shadow-sm">
           <h2 className="text-xl font-semibold mb-4">
             Datos de ventas recientes
           </h2>
-          <div className="overflow-x-auto">
-            <table className="w-full border-collapse">
+          <div className="overflow-x-auto rounded-md">
+            <table className="w-full border-collapse ">
               <thead>
-                <tr className="bg-slate-100">
-                  <th className="border px-4 py-2 text-left">ID</th>
-                  <th className="border px-4 py-2 text-left">Monto</th>
-                  <th className="border px-4 py-2 text-left">Vendedor</th>
-                  <th className="border px-4 py-2 text-left">Fecha</th>
-                  <th className="border px-4 py-2 text-left">Comisión</th>
+                <tr className="bg-gray-100">
+                  <th className="px-4 py-2 text-left">ID</th>
+                  <th className="px-4 py-2 text-left">Monto</th>
+                  <th className="px-4 py-2 text-left">Vendedor</th>
+                  <th className="px-4 py-2 text-left">Fecha</th>
+                  <th className="px-4 py-2 text-left">Comisión</th>
                 </tr>
               </thead>
               <tbody>
                 {deals.map((deal) => (
-                  <tr key={deal.id} className="hover:bg-slate-50">
+                  <tr key={deal.id} className="hover:bg-gray-50">
                     <td className="border px-4 py-2">{deal.id}</td>
-                    <td className="border px-4 py-2">${deal.amount * 0.1}</td>
+                    <td className="border px-4 py-2">${deal.amount}</td>
                     <td className="border px-4 py-2">{deal.salesperson}</td>
                     <td className="border px-4 py-2">
                       {new Date(deal.date).toLocaleDateString()}
@@ -54,11 +56,11 @@ export default function Home() {
                 ))}
               </tbody>
             </table>
-            <div className="mt-4 text-right mr-1">
-              <h2 className="text-xl font-bold">
-                Total Comisiones: ${totalCommissions.toLocaleString()}
-              </h2>
-            </div>
+          </div>
+          <div className="mt-4 text-right mr-1">
+            <h2 className="text-xl font-bold">
+              Total Comisiones: ${totalCommissions.toLocaleString()}
+            </h2>
           </div>
         </div>
       </div>
